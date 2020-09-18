6ee899aa4aa9337fc5610b227d6515722676432f
StormieGal <tfeujio@gmail.com>
2020-09-18 13:51:28 +0100

    mobile version with portrait  done

--- js/index.js
+++ js/index.js
@@ -1,15 +1,42 @@
 const arrow = document.querySelector(".arrow");
 const rowMobile = document.querySelector(".row-mobile");
+let activeIsShown = false;
 arrow.addEventListener("click", displaySocialMediaLinks);
+
+const picture = document.querySelector(".row-mobile picture");
+const row = document.querySelector(".row-mobile .row");
+const arrowImg = document.querySelector(".row-mobile img.arrow");
+const active = document.querySelector(".active");
+
 function displaySocialMediaLinks() {
-  const picture = document.querySelector(".row-mobile picture");
+  // Modify the DOM
+  rowMobile.style.margin = "0 0";
+
   picture.style.display = "none";
-  const row = document.querySelector(".row-mobile .row");
+
   row.style.display = "none";
-  const svg = document.querySelector(".row-mobile svg");
-  svg.style.display = "none";
-  const active = document.querySelector(".active");
+
+  arrowImg.style.display = "none";
+
   active.style.display = "flex";
-  // active.style.backgroundColor = "red";
+  active.style.width = "100%";
+  activeIsShown = true;
+}
+
+const backToNormal = document.getElementById("backToNormal");
+backToNormal.addEventListener("click", redisplayNormal);
+
+function redisplayNormal() {
+  rowMobile.style.margin = "auto 2em";
+
+  picture.style.display = "block";
+
+  row.style.display = "flex";
+
+  arrowImg.style.display = "block";
+
+  active.style.display = "none";
   active.style.width = "100%";
 }
+
+// TO DO: Improve the transistion from normal to .active with an animation.
